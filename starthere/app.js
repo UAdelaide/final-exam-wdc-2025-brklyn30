8feae3d637b0283507646982fcdbc27282fff043
brklyn30 <brooklyndinh0908@gmail.com>
2025-06-20 05:34:37 +0000

    [automatic commit] 2025-06-20 05:34:37 UTC 203.206.5.193

--- starthere/app.js
+++ starthere/app.js
@@ -23,7 +23,7 @@ let db;
     });
 
     // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
+    await connection.query('CREATE DATABASE IF NOT EXISTS test1');
     await connection.end();
 
     // Now connect to the created database
