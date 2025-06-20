16b9300df4e79a2be30fb1611269d87eaae6b100
brklyn30 <brooklyndinh0908@gmail.com>
2025-06-20 11:06:21 +0000

    [automatic commit] 2025-06-20 11:06:21 UTC 203.206.5.193

--- part2/app.js
+++ part2/app.js
@@ -23,11 +23,11 @@ app.use(session({
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-const dogRoutes = require('./routes')
+const dogRoutes = require('./routes/dogRoutes.js');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/dogs', dogsRoutes);
+app.use('/api/dogs', dogRoutes);
 
 // Login Logic for login authentication
 const users = [
