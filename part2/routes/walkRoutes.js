d5614805573917d8b9a910e115da1eed117ff4bf
brklyn30 <brooklyndinh0908@gmail.com>
2025-06-20 10:07:05 +0000

    [automatic commit] 2025-06-20 10:07:04 UTC 203.206.5.193

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -4,6 +4,10 @@ const db = require('../models/db');
 
 // GET all walk requests (for walkers to view)
 router.get('/dogs', async (req, res) => {
+  const user = req.session.user;
+  if (!user || user.role !== 'owner') {
+    return res.status(403).json({ error: 'Unauthorized' });
+  }
   try {
     const [rows] = await db.query(`
       SELECT wr.*, d.name AS dog_name, d.size, u.username AS owner_name
