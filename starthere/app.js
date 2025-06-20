caf18d3c71e563bb15d995ffa140c208cb6e0d82
brklyn30 <brooklyndinh0908@gmail.com>
2025-06-20 04:51:37 +0000

    [automatic commit] 2025-06-20 04:51:36 UTC 203.206.5.193

--- starthere/app.js
+++ starthere/app.js
@@ -47,13 +47,13 @@ let db;
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
     if (rows[0].count === 0) {
       await db.execute(`
-      INSERT INTO Users (username, email, password_hash, role) VALUES
-      ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-      ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-      ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-      ('danwalker', 'dan@example.com', 'passdan', 'walker'),
-      ('eveowner', 'eve@example.com', 'passeve', 'owner')
-    `);
+        INSERT INTO Users (username, email, password_hash, role) VALUES
+        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+        ('danwalker', 'dan@example.com', 'passdan', 'walker'),
+        ('eveowner', 'eve@example.com', 'passeve', 'owner')
+      `);
     }
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
