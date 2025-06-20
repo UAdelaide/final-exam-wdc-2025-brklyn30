fa7c7b99db74688220c3e0a5beec67dcd38e227c
brklyn30 <brooklyndinh0908@gmail.com>
2025-06-20 08:08:33 +0000

    [automatic commit] 2025-06-20 08:08:32 UTC 203.206.5.193

--- part2/app.js
+++ part2/app.js
@@ -12,6 +12,8 @@ app.use(express.static(path.join(__dirname, '/public')));
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
+
+
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
