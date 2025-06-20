fcc07b9c73178fe8804e81ea8746b5637c5e123e
brklyn30 <brooklyndinh0908@gmail.com>
2025-06-20 08:16:49 +0000

    [automatic commit] 2025-06-20 08:16:49 UTC 203.206.5.193

--- part2/app.js
+++ part2/app.js
@@ -3,7 +3,8 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const session
+//
+const session = require('express-session');
 
 // Middleware
 app.use(express.json());
