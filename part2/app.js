6ed361661ead1eef27a39065f432fc6b5acd397e
brklyn30 <brooklyndinh0908@gmail.com>
2025-06-20 12:31:55 +0000

    [automatic commit] 2025-06-20 12:31:55 UTC 203.206.5.193

--- part2/app.js
+++ part2/app.js
@@ -34,7 +34,6 @@ app.use('/api/dogs', dogRoutes);
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
-
   try {
     
 const [rows] = await db.query('SELECT user_id, username, password_hash, role FROM Users WHERE username = ?', [username]);
