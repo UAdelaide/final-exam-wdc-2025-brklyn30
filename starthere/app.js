81a0ba39617323cbc26496566f0c75e4a092be7a
brklyn30 <brooklyndinh0908@gmail.com>
2025-06-20 05:35:39 +0000

    [automatic commit] 2025-06-20 05:35:39 UTC 203.206.5.193

--- starthere/app.js
+++ starthere/app.js
@@ -53,10 +53,10 @@ let db;
 
 
     // Insert data if table is empty
-    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
+    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM dogs');
     if (rows[0].count === 0) {
       await db.execute(`
-        INSERT INTO Users (username, email, password_hash, role) VALUES
+        INSERT INTO dogs (username, email, password_hash, role) VALUES
         ('alice123', 'alice@example.com', 'hashed123', 'owner'),
         ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
         ('carol123', 'carol@example.com', 'hashed789', 'owner'),
