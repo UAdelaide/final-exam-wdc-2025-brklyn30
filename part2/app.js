40c7242454b78a0e5afeedc04c4044108f61c2c2
brklyn30 <brooklyndinh0908@gmail.com>
2025-06-20 08:13:56 +0000

    [automatic commit] 2025-06-20 08:13:56 UTC 203.206.5.193

--- part2/app.js
+++ part2/app.js
@@ -8,6 +8,13 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'walksecret',
+  resave: false,
+  saveUninitialized: false
+}));
+
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
