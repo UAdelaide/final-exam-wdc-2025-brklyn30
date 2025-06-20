6428b6466fb040af5cba86c65c7e53541aca65bf
brklyn30 <brooklyndinh0908@gmail.com>
2025-06-20 08:42:10 +0000

    [automatic commit] 2025-06-20 08:42:10 UTC 203.206.5.193

--- part2/app.js
+++ part2/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-const pool = require('../models');
+const pool = require('../models/database');
 
 const app = express();
 
