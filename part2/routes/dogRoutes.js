0828e80a02150415058fac48af9b59662119a0c8
brklyn30 <brooklyndinh0908@gmail.com>
2025-06-20 11:36:10 +0000

    [automatic commit] 2025-06-20 11:36:09 UTC 203.206.5.193

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -2,16 +2,14 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-router.get('/dogs', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const [rows] = await db.query('SELECT dog_id, owner_id, name, size FROM Dogs');
-    console.log('Dogs fetched:', rows);
     res.json(rows);
   } catch (error) {
     console.error('Error fetching dogs:', error);
     res.status(500).json({ error: 'Failed to fetch dogs' });
   }
 });
-
 
 module.exports = router;
