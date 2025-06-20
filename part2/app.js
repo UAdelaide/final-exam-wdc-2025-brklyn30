f40876c436d8319470666c612182ed501ce9a91b
brklyn30 <brooklyndinh0908@gmail.com>
2025-06-20 08:20:48 +0000

    [automatic commit] 2025-06-20 08:20:47 UTC 203.206.5.193

--- part2/app.js
+++ part2/app.js
@@ -11,7 +11,7 @@ const session = require('express-session');
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
-// Parse Ur
+// Parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
   secret: 'walksecret',
