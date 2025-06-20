29b347bc00ab3df0ef49c6e2376785aa15f25991
brklyn30 <brooklyndinh0908@gmail.com>
2025-06-20 05:01:08 +0000

    [automatic commit] 2025-06-20 05:01:07 UTC 203.206.5.193

--- starthere/app.js
+++ starthere/app.js
@@ -82,10 +82,14 @@ let db;
 // Route to return books as JSON
 app.get('/api/dogs', async (req, res) => {
   try {
-    const [rows] = await db.execute('SELECT * FROM books');
-    res.json(books);
+    const [rows] = await db.execute(`
+      SELECT d.name AS dog_name, d.size, u.username AS owner_username
+      FROM Dogs d
+      JOIN Users u ON d.owner_id = u.user_id
+    `);
+    res.json(dogs);
   } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch books' });
+    res.status(500).json({ error: 'Failed to fetch dogs' });
   }
 });
 
