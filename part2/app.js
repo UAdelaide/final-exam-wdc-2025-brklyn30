79a8d061d16ebd5da2f3d15891e385dd50fd973d
brklyn30 <brooklyndinh0908@gmail.com>
2025-06-20 08:15:01 +0000

    [automatic commit] 2025-06-20 08:15:01 UTC 203.206.5.193

--- part2/app.js
+++ part2/app.js
@@ -19,10 +19,34 @@ app.use(session({
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
-
-
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
+
+// Login Logic
+const users = [
+  { username: 'owner1', password: '123', role: 'owner' },
+  { username: 'walker1', password: '456', role: 'walker' }
+];
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(u => u.username === username && u.password === password);
+
+  if (!user) {
+    return res.status(401).send('Invalid credentials');
+  }
+
+  req.session.user = {
+    username: user.username,
+    role: user.role
+  };
+
+  if (user.role === 'owner') {
+    return res.redirect('/owner-dashboard.html');
+  } else {
+    return res.redirect('/walker-dashboard.html');
+  }
+});
 
 // Export the app instead of listening here
 module.exports = app;
