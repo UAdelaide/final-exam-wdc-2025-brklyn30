611a2c57d05c5994910d0360418f45e8e8d5b211
brklyn30 <brooklyndinh0908@gmail.com>
2025-06-20 05:01:49 +0000

    [automatic commit] 2025-06-20 05:01:49 UTC 203.206.5.193

--- starthere/app.js
+++ starthere/app.js
@@ -82,7 +82,7 @@ let db;
 // Route to return books as JSON
 app.get('/api/dogs', async (req, res) => {
   try {
-    const [rows] = await db.execute(`
+    const [dogs] = await db.execute(`
       SELECT d.name AS dog_name, d.size, u.username AS owner_username
       FROM Dogs d
       JOIN Users u ON d.owner_id = u.user_id
